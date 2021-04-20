8cf1218e10e99e79ff3b643c1c44eaf859d1cd1c
Valentin Brosseau <c4software@gmail.com>
2021-04-20 17:22:13 +0200

    Add a new test

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -6,7 +6,14 @@ test('uses mounts', async () => {
   const wrapper = mount(App)
   expect(wrapper.html()).toContain('Vue app')
   expect(wrapper.html()).toContain('Count: 0')
+})
+
+test('Test click actions', async () => {
+  const wrapper = mount(App)
 
   await wrapper.find('button').trigger('click')
   expect(wrapper.html()).toContain('Count: 1')
+
+  await wrapper.find('button').trigger('click')
+  expect(wrapper.html()).toContain('Count: 2')
 })
